dda0eee3c6698841eba4f2b4cc5498e4572ff22d
abbasexploit <89308347+abbasexploit@users.noreply.github.com>
2021-09-10 08:08:16 +0430

    Update Hash.py

--- Hash.py
+++ Hash.py
@@ -1,1 +1,23 @@
+import hashlib
 
+import Banner
+
+import pyfiglet
+
+def banner():
+
+    abbas = pyfiglet.figlet_format('@abbas.exploit', font= 'big')
+
+    print (abbas)
+
+    print("-----------------------------------------------------------------------------------------------")
+
+banner()
+
+hashvalue = input ("Enter is name : ")
+
+hashob1 = hashlib.md5()
+
+hashob1.update(hashvalue.encode())
+
+print (hashob1.hexdigest())
